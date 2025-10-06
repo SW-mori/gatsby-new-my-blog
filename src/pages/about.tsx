0b4d74dffe835190e9b028c0bc5262e61f2e5ba2
SW-mori <tmori@skywill.jp>
2025-10-06 16:40:02 +0900

    feat: apply the created component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
+import type { PageProps } from "gatsby";
+import { Layout, SEO } from "../components";
 
-const AboutPage: React.FC = () => {
+const AboutPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <h1>About Page</h1>
-      <p>このページは About ページです。</p>
-    </main>
+    <Layout pageTitle="About ページ">
+      <p>このサイトは Gatsby の学習用に作成されています。</p>
+    </Layout>
   );
 };
 
 export default AboutPage;
+
+export const Head = () => (
+  <SEO
+    title="About Page"
+    description="Gatsby サイトのAboutページです"
+    image=""
+    pathname="/about"
+  />
+);
