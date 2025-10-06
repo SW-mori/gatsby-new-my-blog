a3acdfcb57513e1cfc72a484cc98b131ba200a79
SW-mori <tmori@skywill.jp>
2025-10-06 17:33:23 +0900

    fix: build error

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 import { SEOProps } from "./types";
 
 export const SEO: React.FC<SEOProps> = ({
@@ -8,44 +7,19 @@ export const SEO: React.FC<SEOProps> = ({
   image,
   pathname,
 }) => {
-  const { site } = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          description
-          siteUrl
-        }
-      }
-    }
-  `);
-
-  const metaTitle = title || site.siteMetadata.title;
-  const metaDescription = description || site.siteMetadata.description;
-  const metaImage = image
-    ? `${site.siteMetadata.siteUrl}${image}`
-    : `${site.siteMetadata.siteUrl}/default-og.jpg`;
-  const url = `${site.siteMetadata.siteUrl}${pathname || ""}`;
-
   return (
     <>
-      <title>
-        {metaTitle} | {site.siteMetadata.title}
-      </title>
-      <meta name="description" content={metaDescription} />
-
-      {/* OGP */}
-      <meta property="og:title" content={metaTitle} />
-      <meta property="og:description" content={metaDescription} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={metaImage} />
-
-      {/* Twitterカード */}
+      <meta property="og:url" content={pathname} />
+      <meta property="og:image" content={image} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:description" content={metaDescription} />
-      <meta name="twitter:image" content={metaImage} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
     </>
   );
 };
