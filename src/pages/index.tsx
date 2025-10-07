f80cc6c3e0559d2d57c41152d71e2e0466bc8f71
SW-mori <tmori@skywill.jp>
2025-10-07 15:54:14 +0900

    feat: display Markdown files in a list on the top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,56 @@
 import { Layout, SEO } from "../components";
-import type { PageProps } from "gatsby";
+import { QueryData } from "./types";
+import { graphql, type PageProps } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
+import * as styles from "./index.module.scss";
 
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage: React.FC<PageProps<QueryData>> = ({ data }) => {
+  const posts = data.allMarkdownRemark.nodes;
   return (
     <Layout pageTitle="ホーム">
-      <p>Gatsbyで画像最適化を試す。</p>
-      <StaticImage
-        src="../images/sample.png"
-        alt="サンプル画像"
-        width={400}
-        height={300}
-        placeholder="blurred"
-      />
+      <div className={styles.container}>
+        <p className={styles.intro}>
+          Gatsbyで画像最適化と記事一覧を試してみましょう。
+        </p>
+        <StaticImage
+          src="../images/sample.png"
+          alt="サンプル画像"
+          width={600}
+          height={400}
+          placeholder="blurred"
+          className={styles.image}
+        />
+        <div className={styles.postList}>
+          {posts.map((post) => (
+            <article key={post.id} className={styles.post}>
+              <h3 className={styles.postTitle}>{post.frontmatter.title}</h3>
+              <p className={styles.postDate}>{post.frontmatter.date}</p>
+              <p className={styles.postExcerpt}>{post.excerpt}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          date(formatString: "YYYY/MM/DD")
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => (
   <SEO
