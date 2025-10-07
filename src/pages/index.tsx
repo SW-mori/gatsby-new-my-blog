97a51bf0d145216620aaaa44d973856fed8c79ed
SW-mori <tmori@skywill.jp>
2025-10-07 15:34:08 +0900

    feat: trying image optimization with Gatsby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import * as React from "react";
-import type { PageProps } from "gatsby";
 import { Layout, SEO } from "../components";
+import type { PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="ホーム">
-      <p>Gatsby + TypeScript で作ったトップページです。</p>
+      <p>Gatsbyで画像最適化を試す。</p>
+      <StaticImage
+        src="../images/sample.png"
+        alt="サンプル画像"
+        width={400}
+        height={300}
+        placeholder="blurred"
+      />
     </Layout>
   );
 };
