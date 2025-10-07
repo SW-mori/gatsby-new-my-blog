7f6909e15c6c55efdfad32c7d9ec16b1fb0ae926
SW-mori <tmori@skywill.jp>
2025-10-07 11:40:27 +0900

    feat: add installed plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,10 +3,15 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "My Gatsby Site",
-    description: "これは Gatsby + TypeScript の学習用サイトです。",
+    description: "これは Gatsby + TypeScript の学習用サイトです",
   },
   graphqlTypegen: true,
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
 
 export default config;
